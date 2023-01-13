57c48f8415f28f871c60299fbc090db3a31ee237
noeblassel <noe.blassel@gmail.com>
2023-01-13 16:12:18 +0100

    crunch_normal_derivatives.jl

--- plot_scripts/crunch_normal_derivatives.jl
+++ plot_scripts/crunch_normal_derivatives.jl
@@ -19,6 +19,9 @@ for β=βmin:dβ:βmax
     l_normal_derivative_u2 = Float64[]
     r_normal_derivative_u2=Float64[]
 
+    λ1s = Float64[]
+    λ2s = Float64[]
+
     for ix = ix_start:ix_end
         println("\t",ix)
         include(joinpath("/libre2/blasseln/QSD_data/dirichlet_data",dir,"beta$(β)_N$(N)_ix$(ix).out"))
@@ -27,7 +30,7 @@ for β=βmin:dβ:βmax
         
         (u1[5] <= 0 ) && (u1 = -u1)
         (u2[5] <= 0) && (u2 = -u2)
-        
+
          n = length(u1)
 
         push!(l_normal_derivative_u1,(u1[2]-u1[1])/h)
@@ -35,6 +38,9 @@ for β=βmin:dβ:βmax
 
         push!(l_normal_derivative_u2,(u2[2]-u2[1])/h)
         push!(r_normal_derivative_u2,(u2[n-1]-u2[n])/h)
+
+        push!(λ1s,first(λs))
+        push!(λ2s,last(λs))
     end
 
     output_file = open(joinpath(output_dir,"$(output_file_prefix)_normal_derivatives_β$(β)_$(dir).out"),"w")
@@ -42,5 +48,7 @@ for β=βmin:dβ:βmax
     println(output_file,"r_normal_derivative_u1=",r_normal_derivative_u1)
     println(output_file,"l_normal_derivative_u2=",l_normal_derivative_u2)
     println(output_file,"r_normal_derivative_u2=",r_normal_derivative_u2)
+    println(output_file,"λ1s=",λ1s)
+    println(output_file,"λ2s=",λ2s)
     close(output_file)
 end
