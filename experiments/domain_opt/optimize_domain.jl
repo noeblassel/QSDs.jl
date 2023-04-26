38e15cefe2064606b8454b02b3e156f217ab55fd
noeblassel <noe.blassel@gmail.com>
2023-04-26 15:53:20 +0200

    add domain opt 1D

--- experiments/domain_opt/optimize_domain.jl
+++ experiments/domain_opt/optimize_domain.jl
@@ -1,81 +1,90 @@
-using LinearAlgebra,Plots
+using LinearAlgebra,Plots, Arpack
 
-include("QSDs.jl")
-include("SplinePotentials.jl")
+base_path="/home/nblassel/Documents/QSDs.jl/"
 
-function optimize_domain!(a0, b0, V, β, N, Ω_hist,obj_hist, N_iter, η0, n_line_search, grad_tol)
+
+include(joinpath(base_path,"QSDs.jl"))
+include(joinpath(base_path,"SplinePotentials.jl"))
+
+function optimize_domain!(a0, b0, V, β, N, Ω_hist,obj_hist,grad_hist, N_iter, η0)
 
     a,b=a0,b0
+    η = η0
+
+    best_J = Inf
 
     for i=1:N_iter
-        println("=== Iteration $i ===")
-        M,B,δM,∂λ=QSDs.build_FEM_matrices_1D(V_periodic,β,range(a,b,N+1))
-        λs,us = eigen(M[1:N-1,1:N-1],B[1:N-1,1:N-1])
-        λ1,λ2=λs[1:2]
-        u1,u2=us[:,1],us[:,2]
+        M,B,δM,∂λ=QSDs.build_FEM_matrices_1D_Neumann(V_periodic,β,range(a,b,N+1))
+        λs,us = eigs(M[2:N-1,2:N-1],B[2:N-1,2:N-1],nev=2,which=:LR,sigma=0.0)
+        λ1,λ2=real.(λs)
+        us = real.(us)
+        u1 = us[:,1]
+        u2 = us[:,2]
+
+        h=(b-a)/N
         
-        ∂aJ = -inv(β*λ1^2)*(λ2*first(u1)^2 - λ1*first(u2)^2) #from shape derivative computations (minus sign accounts for direction of outward normal at a)
-        ∂bJ = inv(β*λ1^2)*(λ2*last(u1)^2-λ1*last(u2)^2)
+        ∂aJ = inv(β*λ2^2)*(λ2*(first(u1)/h)^2 - λ1*(first(u2)/h)^2) #from shape derivative computations (minus sign accounts for direction of outward normal at a)
+        ∂bJ = -inv(β*λ2^2)*(λ2*(last(u1)/h)^2-λ1*(last(u2)/h)^2)
         
         obj = best_obj = (λ2-λ1)/λ1
-        println("\tDomain : ]",a,", ",b,"[")
-        println("\tObjective: ",obj)
+        J = λ1 / λ2
 
-        norm_grad = sqrt(∂aJ^2 + ∂bJ^2)
-        println("\tGradient: ",norm_grad)
-        (norm_grad < grad_tol) && return
+        if J < best_J
+           # println("=== Iteration $i ===")
+            best_J = J
+            best_obj = (λ2 - λ1)/λ1
+            a -= ∂aJ*η
+            b -= ∂bJ*η
 
-        println("\t== LineSearch ==")
-        η=η0
+            #println("\tDomain : ]",a,", ",b,"[")
+           # println("\tObjective: ",obj)
 
-        for i=1:n_line_search # line search to adjust step size
-            println("\t\t step $i, η=$η")
-            a_tent = a + ∂aJ*η
-            b_tent = b + ∂bJ*η
-            M,B,δM,∂λ=QSDs.build_FEM_matrices_1D(V_periodic,β,range(a_tent,b_tent,N+1))
-            λs,us = eigen(M[1:N-1,1:N-1],B[1:N-1,1:N-1])
-            λ1,λ2 = λs[1:2]
-            obj_tent = (λ2-λ1)/λ1
-            println("\t\tObjective :", obj_tent)
+            push!(Ω_hist,[a,b])
+            push!(obj_hist,best_obj)
+            push!(grad_hist,max(abs(∂aJ),abs(∂bJ)))
 
-            if obj_tent < best_obj
-                η /= 2
-                break
-            end
-
-            best_obj = obj_tent
-            η *= 2
-        end
-
-        a += ∂aJ*η
-        b += ∂bJ*η
+            #println("\tGradient: ",max(abs(∂aJ),abs(∂bJ)))
         
-        push!(Ω_hist,[a,b])
-        push!(obj_hist,best_obj)
+            η *= 1.05
+        
+        else
+            η /= 2
+            (η < 1e-25) && return
+        end
+        
     end
 end
 
 data_dir = "/home/nblassel/Documents/QSDs.jl/results_optim/"
 ## double well potential
     critical_points = [0.001, 0.25,0.5,0.75,0.999]
-    heights = [0.0, -1.5 , 0.0, -2.0, 0.0]
+    heights = [0.0, -1.0 , 0.0, -1.0, 0.0]
 
     V,∇V,∇²V = SplinePotentials.spline_potential_derivatives(critical_points,heights,1)
     V_periodic(x)=V(mod(x,1))
     ## 
 
-    β=4.0
+    as = Float64[]
+    bs = Float64[]
 
-    N=300
+
+    N=1200
     core_set_B = [0.73,0.77]
     core_set_A = [0.23,0.27]
 
-    a,b = core_set_A
-    N_iter = 1500
-    η0=0.1
-    Ω_hist=Vector{Float64}[]
-    obj_hist=Float64[]
-    optimize_domain!(a,b,V_periodic,β,N,Ω_hist,obj_hist,N_iter,η0,20,1e-11)
-    Ω_max = Ω_hist[argmax(obj_hist)]
-
-    f=open(joinpath)
+    for (i,β)=enumerate(range(4,6,100))
+        println("$i/100, β=$β")
+        a,b = 0.5,1.0
+        N_iter = 10000000
+        η0=0.1
+        Ω_hist=Vector{Float64}[]
+        obj_hist=Float64[]
+        grad_hist=Float64[]
+        optimize_domain!(a,b,V_periodic,β,N,Ω_hist,obj_hist,grad_hist,N_iter,η0)
+        Ω_max = Ω_hist[argmax(obj_hist)]
+        println("Max objective: ", maximum(obj_hist))
+        println("Optimal domain: ",a," ",b)
+        push!(as,a)
+        push!(bs,b)
+    
+    end
